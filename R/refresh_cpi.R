7f11c1b442212bf813f1b9fcc1996378e4a5a9b5
Matt Cowgill <mattcowgill@gmail.com>
2020-08-31 09:25:32 +1000

    fix timezone

--- R/refresh_cpi.R
+++ R/refresh_cpi.R
@@ -1,10 +1,10 @@
 library(readabs)
 library(readr)
 library(here)
+library(lubridate)
 
 cpi <- readabs::read_cpi()
-now <- as.POSIXct(Sys.time())
-attributes(now)$tzone <- "Australia/Melbourne"
+now <- lubridate::force_tz(Sys.time(), "Australia/Melbourne")
 cpi$updated <- now
 
 readr::write_csv(cpi, path = here::here("data", "cpi.csv"))
