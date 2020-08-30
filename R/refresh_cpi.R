34aefaa391f486d1389b6142a730c8dfe78f21d5
Matt Cowgill <mattcowgill@gmail.com>
2020-08-31 09:15:20 +1000

    refresh_cpi

--- R/refresh_cpi.R
+++ R/refresh_cpi.R
@@ -3,7 +3,8 @@ library(readr)
 library(here)
 
 cpi <- readabs::read_cpi()
-now <- .POSIXct(Sys.time(), "Australia/Melbourne")
+now <- as.POSIXct(Sys.time())
+attributes(now)$tzone <- "Australia/Melbourne"
 cpi$updated <- now
 
 readr::write_csv(cpi, path = here::here("data", "cpi.csv"))
