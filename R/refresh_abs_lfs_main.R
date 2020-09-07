936f4990bcd7a58f9d7f938f2e0ed44a268ca5c8
Matt Cowgill <mattcowgill@gmail.com>
2020-09-07 21:00:30 +1000

    monthly lfs

---
+++ R/refresh_abs_lfs_main.R
@@ -0,0 +1,26 @@
+library(readabs)
+library(dplyr)
+library(here)
+
+raw_path <- here::here("data-raw", "abs")
+
+old_lfs_6202_11s <- read_abs_local("6202.0", "11a", 
+                                   path = raw_path)
+
+new_lfs_6202_11s <- read_abs("6202.0", "11a", check_local = FALSE, 
+                        path = raw_path)
+
+old_date <- max(old_lfs_6202_11s$date)
+new_date <- max(new_lfs_6202_11s$date)
+
+if (new_date > old_date) {
+  lfs_m <- read_abs("6202.0", check_local = FALSE)
+  
+  lfs_m <- lfs_m %>%
+    select(date, series, series_type, series_id, value) %>%
+    filter(!is.na(value)) %>%
+    mutate_if(is.character, as.factor)
+  
+  saveRDS(lfs_m, here::here("data", "abs", "6202.rds"))
+
+}
