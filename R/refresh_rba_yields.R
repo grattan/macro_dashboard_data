0365137c1c90dd867f789f88799de00942667ee2
Matt Cowgill <mattcowgill@gmail.com>
2020-11-26 16:44:56 +1100

    add detailed yields

--- R/refresh_rba_yields.R
+++ R/refresh_rba_yields.R
@@ -2,7 +2,8 @@ library(readrba)
 library(dplyr)
 library(here)
 library(fst)
-
+library(tidyr)
+library(lubridate)
 
 monthly_yields <- read_rba(series_id = 
                              c("FCMYGBAG2",	"FCMYGBAG3",	"FCMYGBAG5",
@@ -16,6 +17,17 @@ daily_yields <- read_rba(series_id =
                              "FCMYGBAG10D",	
                              "FCMYGBAGID"))
 
+detailed_yields <- readrba::read_rba(table_no = c("f16", "f16"),
+                                     cur_hist = c("current", "historical"))
+
+detailed_yields <- detailed_yields %>%
+  filter(!grepl("Indexed", series)) %>%
+  tidyr::separate(description,
+                  into = c("bond_num", "coupon", "maturity_date"),
+                  sep = " - ") %>%
+  mutate(maturity_date = lubridate::dmy(maturity_date)) %>%
+  select(date, value, maturity_date)
+
 fst::write_fst(monthly_yields,
                here::here("data", "rba", "rba_monthly_yields.fst"),
                compress = 100)
@@ -23,6 +35,9 @@ fst::write_fst(monthly_yields,
 fst::write_fst(daily_yields,
                here::here("data", "rba", "rba_daily_yields.fst"),
                compress = 100)
+
+fst::write_fst(detailed_yields,
+               here::here("data", "rba", "rba_detailed_yields.fst"))
 
 readr::read_csv(here::here("last_updated.csv")) %>%
   bind_rows(tibble(data = "rba_yields", date = Sys.time())) %>%
